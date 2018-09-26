211e991347158d2409eb34f087a1f5c9f5dd5879
ilmirons <ilmirons@gmail.com>
2018-09-26 18:39:27 +0200

    minor enhancements. New repo

--- index.js
+++ index.js
@@ -1,51 +1,53 @@
 #!/usr/bin/env node
 
-'use strict'
-
 var fs = require('fs')
 var ejs = require('ejs')
 var _ = require('lodash')
 var path = require('path')
 var mkdirp = require('mkdirp')
   // TODO add cmdLine support for path
-var testRoot = path.normalize(process.cwd() + '/tests')
+var testRoot = path.join(process.cwd(), '/tests')
   // TODO adapt to support json5
-var defaults = require(testRoot + '/default.json');
-var tests = require(testRoot + '/tests.json');
+var defaults = require(path.join(testRoot, '/default.json'))
+var tests = require(path.join(testRoot, '/tests.json'))
 
-var mkInputPath = (name) => path.normalize(testRoot + '/testInputs/' + name + '.gen.json').replace(/\\/g, '\\\\')
-mkdirp(testRoot + '/testInputs/')
+const mkInputPath = (name) => path.join(testRoot, '/testInputs/', name + '.gen.json').replace(/\\/g, '\\\\')
 
-// Endpoint: http://salcls0293.sede.corp.sanpaoloimi.com:9080/DecisionService/rest/JUIB0_MDI/1.0/JUIB0_MDIRule/1.0
-// Request Method: POST
+mkdirp(path.join(testRoot, '/testInputs/'))
 
-var generateTestInput = (t) => {
-  let req = defaults.body;
-  let inputFile = mkInputPath(t.title)
-  t.inputs.forEach((i) => eval(i));
+const generateTestInput = (t) => {
+  const req = defaults.body
+  const inputFile = mkInputPath(t.title)
+  // TODO find a safer alternative to this eval call
+  t.inputs.forEach((i) => eval(i))
   fs.writeFile(inputFile, JSON.stringify(req), (err) => {
-    if (err) throw err;
-    // TODO add cmdLine support for verbose
-    else console.info(inputFile, 'has been generated!');
+    if (err) throw err
+      // TODO add cmdLine support for verbose
+    else console.info(inputFile, 'has been generated!')
   })
 }
 
 var generateTest = (tests) => {
-  let testGroups = _.groupBy(tests, (t) => t.title.split('_')[0]);
-  let eTests = {
-    testGroups: _.mapValues(testGroups, (tests) => {
-      return tests.map((t) => {
-        t.testTitle = t.title.split('_').slice(1).join('_')
-        t.requestOptions = t.requestOptions !== undefined ? _.merge(defaults.requestOptions, t.requestOptions) :
-          defaults.requestOptions;
-        t.filePath = mkInputPath(t.title);
+  const testGroups = _.groupBy(tests, (t) => t.title.split('_')[0])
+  const eTests = {
+    testGroups: _.mapValues(testGroups, (tests) =>
+      tests.map((t) => {
+        if (_.isEmpty(t.title)) throw new Error('Titles for tests can not be empty');
+        t.testTitle = t.title.split('_').slice(t.title.split('_').length > 1 ? 1
+          : 0).join('_')
+        if ([t.requestOptions, defaults.requestOptions].every(_.isEmpty)) {
+          throw new Error('At least one among test or default requestOptions must be defined')
+        }
+        t.requestOptions = _.isObject(t.requestOptions) ?
+          _.merge(defaults.requestOptions, t.requestOptions) : defaults.requestOptions
+        t.filePath = mkInputPath(t.title)
         return t;
-      });
-    })
+      })
+    )
   };
-  ejs.renderFile(__dirname + '/testTemplate.tpl.js', eTests, {}, (err, str) => {
+  ejs.renderFile(path.join(__dirname, '/testTemplate.ejs'), eTests, {}, (err, str) => {
     if (err) throw err;
-    fs.writeFile(testRoot + '/testSuite.gen.js', str, (err) => {
+    fs.writeFile(path.join(testRoot, '/testSuite.gen.js'), str, (err) => {
       if (err) throw err;
       else console.info('testSuite has been generateded!');
     })
