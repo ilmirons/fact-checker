b72c8178725676722976da114668ef667ec8ba63
ilmirons <ilmirons@gmail.com>
2018-10-04 16:15:35 +0200

    Added clean method + json5 parsing. New deps

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
 #!/usr/bin/env node
 
-var fs = require('fs')
-var ejs = require('ejs')
-var _ = require('lodash')
-var path = require('path')
-var mkdirp = require('mkdirp')
-  // TODO add cmdLine support for path
-var testRoot = path.join(process.cwd(), '/tests')
-  // TODO adapt to support json5
-var defaults = require(path.join(testRoot, '/default.json'))
-var tests = require(path.join(testRoot, '/tests.json'))
+const fs       = require('fs')
+const rReadDir = require('recursive-readdir')
+const ejs      = require('ejs')
+const _        = require('lodash')
+const path     = require('path')
+const mkdirp   = require('mkdirp')
+const JSON5    = require('json5')
+
+// TODO add cmdLine support for path
+
+const testRoot = path.join(process.cwd(), '/tests')
+const defaults = JSON5.parse(fs.readFileSync(path.join(testRoot, '/default.json')))
+const tests    = JSON5.parse(fs.readFileSync(path.join(testRoot, '/tests.json')))
 
 const mkInputPath = (name) => path.join(testRoot, '/testInputs/', name + '.gen.json').replace(/\\/g, '\\\\')
 
 mkdirp(path.join(testRoot, '/testInputs/'))
 
+const clean = () => rReadDir(testRoot)
+  .then((files) => 
+    Promise.all(files
+      .filter((f) => /.*\.gen\.\w+/.test(f))
+      .map((f) => new Promise((resolve, reject) => fs.unlink(f,
+        (err) => {
+          if (err) reject(err)
+          else resolve();
+        }))))
+  )
+
+
 const generateTestInput = (t) => {
   const req = defaults.body
   const inputFile = mkInputPath(t.title)
-  // TODO find a safer alternative to this eval call
+    // TODO find a safer alternative to this eval call
   t.inputs.forEach((i) => eval(i))
   fs.writeFile(inputFile, JSON.stringify(req), (err) => {
     if (err) throw err
@@ -27,14 +42,15 @@ const generateTestInput = (t) => {
   })
 }
 
-var generateTest = (tests) => {
+var generateSuite = (tests) => {
   const testGroups = _.groupBy(tests, (t) => t.title.split('_')[0])
   const eTests = {
     testGroups: _.mapValues(testGroups, (tests) =>
       tests.map((t) => {
         if (_.isEmpty(t.title)) throw new Error('Titles for tests can not be empty');
-        t.testTitle = t.title.split('_').slice(t.title.split('_').length > 1 ? 1
-          : 0).join('_')
+        const splitTitle = t.title.split('_');
+        t.testTitle = splitTitle.slice(splitTitle.length > 1 ? 1 :
+          0).join('_')
         if ([t.requestOptions, defaults.requestOptions].every(_.isEmpty)) {
           throw new Error('At least one among test or default requestOptions must be defined')
         }
@@ -54,5 +70,10 @@ var generateTest = (tests) => {
   });
 }
 
-tests.forEach(generateTestInput);
-generateTest(tests)
+clean()
+  .then(() => {
+    tests.forEach(generateTestInput)
+    generateSuite(tests)
+    return true;
+  })
+  .catch((err) => console.err(err));
