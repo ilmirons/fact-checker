dd7f6fa3ec6767467687f26595abc778bf7c4e79
Andrea Mirone <andrea.mirone@eng.it>
2019-01-08 18:07:52 +0100

    Added cli support and save response option

--- index.js
+++ index.js
@@ -6,19 +6,33 @@ const fs       = require('fs')
 const JSON5    = require('json5')
 const mkdirp   = require('mkdirp')
 const path     = require('path')
-// TODO convert everything to promises
 const pify     = require('pify')
 const rReadDir = require('recursive-readdir')
+const program  = require('commander')
 
-// TODO add cmdLine support for path
+program
+  .version(require(path.join(__dirname, 'package.json')).version)
+  .option('-v, --verbose', 'Print more output')
+  .option('-r, --root <path>', 'Test root folder')
+  .option('-d, --defaults', 'Template with default values for messages to be sent')
+  .option('-s, --save', 'Save responses from the server')
+  .option('-t, --tests', 'Input file for test definition')
+  .parse(process.argv)
 
-const testRoot = path.join(process.cwd(), '/tests')
-const defaults = JSON5.parse(fs.readFileSync(path.join(testRoot, '/default.json')))
-const tests    = JSON5.parse(fs.readFileSync(path.join(testRoot, '/tests.json')))
+const isJson = true // program.format && program.format.toLowerCase() === 'json' || true
 
-const mkInputPath = (name) => path.join(testRoot, '/testInputs/', name + '.gen.json').replace(/\\/g, '\\\\')
+const fileExt     = isJson ? 'json' : 'xml'
+const testRoot    = program.root || './tests'
+const defaultFile = program.defaults || path.join(testRoot, `default.${fileExt}`)
+const defaults    = JSON5.parse(fs.readFileSync(defaultFile))
+const testFile    = program.tests || path.join(testRoot, 'tests.json')
+const tests       = JSON5.parse(fs.readFileSync(testFile))
+
+const mkInputPath = (name) => path.join(testRoot, '/testInputs/', `${name}.gen.${fileExt}`).replace(/\\/g, '\\\\')
+const mkOutputPath = (name) => path.join(testRoot, '/testOutputs/', `${name}.${fileExt}`).replace(/\\/g, '\\\\')
 
 mkdirp(path.join(testRoot, '/testInputs/'))
+mkdirp(path.join(testRoot, '/testOutputs/'))
 
 const unlinkPromise = pify(fs.unlink)
 const clean = () => rReadDir(testRoot)
@@ -28,7 +42,6 @@ const clean = () => rReadDir(testRoot)
     )
   )
 
-
 const generateTestInput = (t) => {
   const req = defaults.body
   const inputFile = mkInputPath(t.title)
@@ -36,8 +49,7 @@ const generateTestInput = (t) => {
   t.inputs.forEach((i) => eval(i))
   fs.writeFile(inputFile, JSON.stringify(req), (err) => {
     if (err) throw err
-      // TODO add cmdLine support for verbose
-    else console.info(inputFile, 'has been generated!')
+    else if (program.verbose) { console.info(inputFile, 'has been generated!') }
   })
 }
 
@@ -54,6 +66,8 @@ var generateSuite = (tests) => {
         t.testTitle = splitTitle.slice(splitTitle.length > 1 ? 1 : 0).join('_')
         t.requestOptions = _.merge(defaults.requestOptions, t.requestOptions)
         t.filePath = mkInputPath(t.title)
+        t.outPath = mkOutputPath(t.title)
+        t.saveRes = program.save
         return t;
       })
     )
@@ -62,7 +76,7 @@ var generateSuite = (tests) => {
     if (err) throw err;
     fs.writeFile(path.join(testRoot, '/testSuite.gen.js'), str, (err) => {
       if (err) throw err;
-      else console.info('testSuite has been generateded!');
+      else if (program.verbose) { console.info('testSuite has been generated!') }
     })
   });
 }
