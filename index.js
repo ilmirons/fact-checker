8e097bc22095fc35d1fbbd37931af015e2e8036e
Andrea Mirone <andrea.mirone@eng.it>
2019-01-09 11:52:53 +0100

    Multi file test suites

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ async function getTestDefinitions(fileGlob) {
   return _.flatMap(files.filter(mm.filter(fileGlob, { matchBase: true, nocase: true })),
     (file) => { 
       let ts = JSON5.parse(fs.readFileSync(file)); 
-      return ts.map(t => { t.srcFile = path.basename(file); return t } ) 
+      return ts.map(t => { t.srcFile = path.basename(file).split('.')[0]; return t } ) 
     })
 }
 
@@ -62,40 +62,41 @@ const generateTestInput = (t) => {
   })
 }
 
-var generateSuite = (tests) => {
-  const testGroups = _.groupBy(tests, (t) => t.title.split('_')[0])
-  const eTests = {
-    testGroups: _.mapValues(testGroups, (tests) =>
-      tests.map((t) => {
-        if (_.isEmpty(t.title)) throw new Error('Titles for tests can not be empty');
-        if ([t.requestOptions, defaults.requestOptions].every(_.isEmpty)) {
-          throw new Error('At least one among test or default requestOptions must be defined')
-        }
-        const splitTitle = t.title.split('_');
-        t.testTitle = splitTitle.slice(splitTitle.length > 1 ? 1 : 0).join('_')
-        t.requestOptions = _.merge(defaults.requestOptions, t.requestOptions)
-        t.filePath = mkInputPath(t.title)
-        t.outPath = mkOutputPath(t.title)
-        t.saveRes = program.save
-        return t;
-      })
-    )
-  };
-  ejs.renderFile(path.join(__dirname, '/testTemplate.ejs'), eTests, {}, (err, str) => {
-    if (err) throw err;
-    fs.writeFile(path.join(testRoot, '/testSuite.gen.js'), str, (err) => {
+var generateSuites = (tests) => {
+  _.forOwn(_.groupBy(tests, (t) => t.srcFile), (tests, fileName) => {
+    const testGroups = _.groupBy(tests, (t) => t.title.split('_')[0])
+    const eTests = {
+      testGroups: _.mapValues(testGroups, (tests) =>
+        tests.map((t) => {
+          if (_.isEmpty(t.title)) throw new Error('Titles for tests can not be empty');
+          if ([t.requestOptions, defaults.requestOptions].every(_.isEmpty)) {
+            throw new Error('At least one among test or default requestOptions must be defined')
+          }
+          const splitTitle = t.title.split('_');
+          t.testTitle = splitTitle.slice(splitTitle.length > 1 ? 1 : 0).join('_')
+          t.requestOptions = _.merge(defaults.requestOptions, t.requestOptions)
+          t.filePath = mkInputPath(t.title)
+          t.outPath = mkOutputPath(t.title)
+          t.saveRes = program.save
+          return t;
+        })
+      )
+    };
+    ejs.renderFile(path.join(__dirname, '/testTemplate.ejs'), eTests, {}, (err, str) => {
       if (err) throw err;
-      else if (program.verbose) { console.info('testSuite has been generated!') }
-    })
-  });
+      fs.writeFile(path.join(testRoot, `/${fileName}Suite.gen.js`), str, (err) => {
+        if (err) throw err;
+        else if (program.verbose) { console.info('testSuite has been generated!') }
+      })
+    });
+  })
 }
 
 clean()
   .then(() => getTestDefinitions(testFileGlob))
   .then((tests) => {
-    console.log(tests)
     tests.forEach(generateTestInput)
-    generateSuite(tests)
+    generateSuites(tests)
     return true;
   })
   .catch((err) => console.error(err.message));
